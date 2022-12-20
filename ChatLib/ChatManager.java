2d111d3584ef4f854ca08376cf4123c95683ff23
Anatoly <107132732+Seven7897@users.noreply.github.com>
2022-12-20 19:59:13 +0300

    Create ChatManager.java

---
+++ ChatLib/ChatManager.java
@@ -0,0 +1,18 @@
+package MessengerApp.ChatLib;
+
+import MessengerApp.UserLib.User;
+
+public class ChatManager {
+
+    public void enterTheChat(Chatroom chatroom, User user) {
+        chatroom.addUser(user);
+    }
+
+    public void leaveTheChat(Chatroom chatroom, User user) {
+        chatroom.removeUser(user);
+    }
+
+    public void getMessage(Chatroom chatroom, User user, String msg) {
+        chatroom.printMessage(user, msg);
+    }
+}
